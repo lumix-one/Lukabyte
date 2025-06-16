d738d9ff5f1e5c205ce5a40bdd0778d712808d52
Lucifer-v1 <purificateurlange@gmail.com>
2025-06-16 08:36:33 +0700

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,19 +14,19 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100065176744106"];
+    const permission = ["61554451689144"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("𝙎𝙊𝙍𝙍𝙔 𝙊𝙉𝙇𝙔 [ 🧛𝘿𝙍𝘼𝘾𝙐𝙇𝘼 🧛]\n_____________________\n can use file {😶}", event.threadID, event.messageID);
+      return api.sendMessage("𝙎𝙊𝙍𝙍𝙔 𝙊𝙉𝙇𝙔 [ 🍀🩸 𝑀𝑟 𝐿𝑈𝐶𝐼𝐹𝐸𝑅 🩸🍀]\n_____________________\n 𝗖𝗔𝗡 𝗨𝗦𝗘 𝗙𝗜𝗟𝗘 {🩸🍀}", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("𝙇𝙀 𝙉𝙊𝙈 𝘿𝙐 𝙁𝙄𝘾𝙃𝙄𝙀𝙍 💢.", event.threadID, event.messageID);
+      return api.sendMessage("𝙇𝙀 𝙉𝙊𝙈 𝘿𝙐 𝙁𝙄𝘾𝙃𝙄𝙀𝙍 𝑀𝑟 🍀🩸 𝐿𝑈𝐶𝐼𝐹𝐸𝑅 🩸🍀.", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`💢|𝙁𝙄𝘾𝙃𝙄𝙀𝙍 𝙄𝙉𝙏𝙍𝙊𝙐𝙑𝘼𝘽𝙇𝙀 𝘽𝙊𝙎𝙎 😿: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`🤧|𝙁𝙄𝘾𝙃𝙄𝙀𝙍 𝙄𝙉𝙏𝙍𝙊𝙐𝙑𝘼𝘽𝙇𝙀 𝘽𝙊𝙎𝙎 😿: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');

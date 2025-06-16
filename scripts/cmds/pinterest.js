ecee1e6f7ce4536b6680a0f63f630fbd85a35a46
Lucifer-v1 <purificateurlange@gmail.com>
2025-06-16 09:25:31 +0700

    Update pinterest.js

--- scripts/cmds/pinterest.js
+++ scripts/cmds/pinterest.js
@@ -36,7 +36,7 @@ module.exports = {
   {
  
     const keySearch = args.join(" ");
-    if(keySearch.includes("-") == false) return api.sendMessage('🧛𝘿𝙍𝘼𝘾𝙐𝙇𝘼🧛\n_______________________________\n 𝙀𝙭𝙚𝙢𝙥𝙡𝙚 : 𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝙩 𝙋𝘼𝙄𝙉 - 10\n (Ç𝐀 𝐃𝐄𝐏𝐀𝐍𝐃 𝐃𝐔 𝐍𝐎𝐌𝐁𝐑𝐄 𝐃.𝐈𝐌𝐀𝐆𝐄 𝐐𝐔𝐄 𝐕𝐎𝐔𝐒 𝐕𝐎𝐔𝐋𝐄𝐙 )🧑‍🦯💔🏴‍☠️', event.threadID, event.messageID)
+    if(keySearch.includes("-") == false) return api.sendMessage('🍀🩸 𝑀𝑂𝑅𝑁𝐼𝑁𝐺 𝑆𝑇𝐴𝑅 🩸🍀\n_______________________________\n 𝙀𝙭𝙚𝙢𝙥𝙡𝙚 : 𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝘁 🩸 𝗟𝗨𝗖𝗜𝗙𝗘𝗥 🩸 - 10\n (Ç𝐀 𝐃𝐄𝐏𝐀𝐍𝐃 𝐃𝐔 𝐍𝐎𝐌𝐁𝐑𝐄 𝐃.𝐈𝐌𝐀𝐆𝐄 𝐐𝐔𝐄 𝐕𝐎𝐔𝐒 𝐕𝐎𝐔𝐋𝐄𝐙 )🧸💧🐒', event.threadID, event.messageID)
     const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
     let numberSearch = keySearch.split("-").pop() || 6
     if(numberSearch>20){
@@ -54,7 +54,7 @@ module.exports = {
     }
     api.sendMessage({
         attachment: imgData,
-        body: numberSearch + '-- 𝙍𝙀𝙎𝙐𝙇𝙏 --🧛:\n '+ keySearchs
+        body: numberSearch + '◧💧 𝗩𝗢𝗜𝗖𝗜 𝗩𝗢𝗦 𝗥𝗘𝗦𝗨𝗟𝗧𝗔𝗧𝗦 𝗕𝗢𝗦𝗦 💧◨:\n '+ keySearchs
     }, event.threadID, event.messageID)
     for (let ii = 1; ii < parseInt(numberSearch); ii++) {
         fs.unlinkSync(__dirname + `/tmp/${ii}.jpg`)
